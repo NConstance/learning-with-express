913ff7b5162b7141800a48e61040dfa31ea29c5f
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-08-29 13:26:01 -0400

    restructured the routes

---
+++ index.js
@@ -0,0 +1,11 @@
+// create your own server with a choosen database
+
+// create a get request
+
+// create get by id request
+
+// create a post request
+
+// creat a put request
+
+// create a delete
--- server.js
+++ server.js
@@ -35,78 +35,159 @@ app.get("/", (req, res) => {
   res.json("Hi, welcome to our server.");
 });
 
-app.get("/users", (req, res) => {
-  // retrun the users data
-  res.json(userData);
-});
+app
+  .route("/users")
+  .get((req, res) => {
+    // retrun the users data
+    res.json(userData);
+  })
+  .post((req, res) => {
+    try {
+      // grab the data the client enter through req.body
+      let data = req.body;
+
+      // push data from client into the userData
+      userData.push(data);
+      res.json(userData);
+    } catch (err) {
+      console.log("Error: ", err);
+      res.json("An error has occurred");
+    }
+  });
+
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    // we grab the id the client entered from the parameters
+    try {
+      let id = req.params.id;
+
+      // we access our array of objects to find the id entered.
+      let result = userData.filter((i) => i.id == id);
+      res.json(result);
+    } catch (err) {
+      console.log("Error: ", err);
+      res.json("An error has occurred");
+    }
+  })
+  .put((req, res) => {
+    try {
+      //   res.json("This is a put request");
+      // get the id of the data the client wants to change/update
+      let id = req.params.id;
+      let data = req.body;
+
+      // find the data that matches the id entered by the client
+      let oldDataIndex = userData.findIndex((i) => i.id == id);
+      //   res.json(oldData);
+
+      // change/update the data
+      userData[oldDataIndex] = data;
+
+      // send data back to client
+      res.json(userData[oldDataIndex]);
+      //   console.log(userData);
+    } catch (err) {
+      console.log("Error:", err);
+      res.json("An error has occurred");
+    }
+  })
+  .delete((req, res) => {
+    try {
+      //   res.json("This is a delete request");
+      // grab the id of the data the client wants to delete
+      let id = req.params.id;
+      // find the index of the data
+      let oldDataIndex = userData.findIndex((i) => i.id == id);
+      // remove the data
+      userData.splice(oldDataIndex, 1);
+
+      res.json(`${id} has been delated current database`);
+
+      console.log(userData);
+    } catch (err) {
+      console.log("Error:", err);
+      res.json("An error has occurred");
+    }
+  });
+
+// app.get("/users", (req, res) => {
+//   // retrun the users data
+//   res.json(userData);
+// });
+
+// // post request - create new data
+// app.post("/users", (req, res) => {
+//   try {
+//     // grab the data the client enter through req.body
+//     let data = req.body;
+
+//     // push data from client into the userData
+//     userData.push(data);
+//     res.json(userData);
+//   } catch (err) {
+//     console.log("Error: ", err);
+//     res.json("An error has occurred");
+//   }
+// });
 
 // create a get route to only get one rout based on the id
-app.get("/users/:id", (req, res) => {
-  // we grab the id the client entered from the parameters
-  try {
-    let id = req.params.id;
+// app.get("/users/:id", (req, res) => {
+//   // we grab the id the client entered from the parameters
+//   try {
+//     let id = req.params.id;
 
-    // we access our array of objects to find the id entered.
-    let result = userData.filter((i) => i.id == id);
-    res.json(result);
-  } catch (err) {
-    console.log("Error: ", err);
-    res.json("An error has occurred");
-  }
-});
+//     // we access our array of objects to find the id entered.
+//     let result = userData.filter((i) => i.id == id);
+//     res.json(result);
+//   } catch (err) {
+//     console.log("Error: ", err);
+//     res.json("An error has occurred");
+//   }
+// });
 
-// post request - create new data
-app.post("/users", (req, res) => {
-  try {
-    // grab the data the client enter through req.body
-    let data = req.body;
+// // put request- updating exist data
+// app.put("/users/:id", (req, res) => {
+//   try {
+//     //   res.json("This is a put request");
+//     // get the id of the data the client wants to change/update
+//     let id = req.params.id;
+//     let data = req.body;
 
-    // push data from client into the userData
-    userData.push(data);
-    res.json(userData);
-  } catch (err) {
-    console.log("Error: ", err);
-    res.json("An error has occurred");
-  }
-});
+//     // find the data that matches the id entered by the client
+//     let oldDataIndex = userData.findIndex((i) => i.id == id);
+//     //   res.json(oldData);
 
-// put request- updating exist data
-app.put("/users/:id", (req, res) => {
-  //   res.json("This is a put request");
-  // get the id of the data the client wants to change/update
-  let id = req.params.id;
-  let data = req.body;
+//     // change/update the data
+//     userData[oldDataIndex] = data;
 
-  // find the data that matches the id entered by the client
-  let oldDataIndex = userData.findIndex((i) => i.id == id);
-  //   res.json(oldData);
+//     // send data back to client
+//     res.json(userData[oldDataIndex]);
+//     //   console.log(userData);
+//   } catch (err) {
+//     console.log("Error:", err);
+//     res.json("An error has occurred");
+//   }
+// });
+// // delete request- deleting esixting
+// app.delete("/users/:id", (req, res) => {
+//   try {
+//     //   res.json("This is a delete request");
+//     // grab the id of the data the client wants to delete
+//     let id = req.params.id;
+//     // find the index of the data
+//     let oldDataIndex = userData.findIndex((i) => i.id == id);
+//     // remove the data
+//     userData.splice(oldDataIndex, 1);
 
-  // change/update the data
-  userData[oldDataIndex] = data;
+//     res.json(`${id} has been delated current database`);
 
-  // send data back to client
-  res.json(userData[oldDataIndex]);
-  //   console.log(userData);
-});
-// delete request- deleting esixting
-app.delete("/users/:id", (req, res) => {
-  try {
-    //   res.json("This is a delete request");
-    // grab the id of the data the client wants to delete
-    let id = req.params.id;
-    // find the index of the data
-    let oldDataIndex = userData.findIndex((i) => i.id == id);
-    // remove the data
-    userData.splice(oldDataIndex, 1);
-
-    res.json(`${id} has been delated current database`);
-
-    console.log(userData);
-  } catch (err) {
-    console.log("Error:", err);
-    res.json("An error has occurred");
-  }
-});
+//     console.log(userData);
+//   } catch (err) {
+//     console.log("Error:", err);
+//     res.json("An error has occurred");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
